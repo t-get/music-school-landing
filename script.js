c137f681fff4fb5e6394c1492f570afd006bece9
t-get <timofey.acc@gmail.com>
2026-05-09 00:42:20 +0300

    Второая версия дополненная

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log("Лендинг загружен");
+const revealItems = document.querySelectorAll(".reveal");
+
+const revealOnScroll = () => {
+  revealItems.forEach((item) => {
+    const rect = item.getBoundingClientRect();
+    if (rect.top < window.innerHeight - 80) {
+      item.classList.add("is-visible");
+    }
+  });
+};
+
+revealOnScroll();
+window.addEventListener("scroll", revealOnScroll);
